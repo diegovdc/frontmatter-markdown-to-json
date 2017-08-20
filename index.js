d73611f13ceb72d31469c5b1bc0920875153e120
Diego <diego.vid.eco@gmail.com>
2017-08-20 12:59:08 -0500

    funcionando!

---
+++ index.js
@@ -0,0 +1,38 @@
+var fs = require('fs')
+  , fm = require('front-matter')
+  , marked = require('marked');
+ const glob = require('glob')
+ const R = require('ramda')
+ const {readAsText, writeFile} = require('coral-fs-tasks')
+ const Task = require('data.task')
+const getDir = dir => glob.sync(`./content/**/*.md`)
+
+let parse = R.compose(
+	content => R.assoc('body', marked(content.body), content),
+	fm
+)
+let groupFilesByFolder = R.compose(
+	R.map(R.map(x => parse(x.content))),
+	R.groupBy(x => x.folder)
+)
+
+let fileTask = path => R.composeK(
+	file => Task.of({folder: path.split('/').slice(2)[0], content: file}),
+	readAsText('utf8')
+)(path)
+
+
+
+const writeJsons = obj => 
+	R.sequence(
+		Task.of, 
+		R.map(key => 
+			writeFile('./', {filename:key+'.json' ,buffer:JSON.stringify(obj[key])}), 
+		Object.keys(obj))
+	)
+
+
+let files = R.sequence(Task.of, R.map(fileTask, getDir()))
+	.map(groupFilesByFolder)
+	.chain(writeJsons)
+	.fork(console.log, s => console.log('Alles Gut!'))
