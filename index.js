3e756c2e091151779c3b5397cf422926c4ca6455
Diego <diego.vid.eco@gmail.com>
2017-09-18 19:46:30 -0500

    adds readme and gitignore

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-var fs = require('fs')
-  , fm = require('front-matter')
-  , marked = require('marked');
- const glob = require('glob')
- const R = require('ramda')
- const {readAsText, writeFile} = require('coral-fs-tasks')
- const Task = require('data.task')
+const fs = require('fs')
+const fm = require('front-matter')
+const marked = require('marked');
+const glob = require('glob')
+const R = require('ramda')
+const {readAsText, writeFile} = require('coral-fs-tasks')
+const Task = require('data.task')
 const getDir = dir => glob.sync(`./content/**/*.md`)
+const formatJson = require('format-json-pretty');
 
 let parse = R.compose(
 	content => R.assoc('body', marked(content.body), content),
 	fm
 )
+
 let groupFilesByFolder = R.compose(
 	R.map(R.map(x => parse(x.content))),
 	R.groupBy(x => x.folder)
@@ -20,8 +22,6 @@ let fileTask = path => R.composeK(
 	file => Task.of({folder: path.split('/').slice(2)[0], content: file}),
 	readAsText('utf8')
 )(path)
-
-
 
 const writeJsons = obj => 
 	R.sequence(
---
+++ readme.md
@@ -0,0 +1,5 @@
+# frontmatter-markdown-to-json
+
+Converts a `frontmatter markdown` collection of files into a json file.
+
+## CLI usage
