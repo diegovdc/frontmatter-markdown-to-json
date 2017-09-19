d2b3905d9b78175131c9390981218457ed996ebf
Diego <diego.vid.eco@gmail.com>
2017-09-18 20:11:58 -0500

    wip

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const fs = require('fs')
+const path = require('path')
 const fm = require('front-matter')
 const marked = require('marked');
 const glob = require('glob')
 const R = require('ramda')
 const {readAsText, writeFile} = require('coral-fs-tasks')
 const Task = require('data.task')
-const getDir = dir => glob.sync(`./content/**/*.md`)
 const formatJson = require('format-json-pretty');
+
+const input_dir = R.compose(
+	s => path.join(process.cwd(), s),
+	s => s.replace('--in=', ''),
+	xs => xs[0] !== undefined && xs[0] !== '--in=' ? xs[0] : './content/',
+	R.filter(s => s.indexOf('--in=') === 0)
+)(process.argv)
+
+console.log("input_dir", input_dir);
+const output_dir = R.compose(
+	s => path.join(process.cwd(), s),
+	s => s.replace('--out=', ''),
+	xs => xs[0] !== undefined && xs[0] !== '--out=' ? xs[0] : './',
+	R.filter(s => s.indexOf('--out=') === 0)
+)(process.argv)
+
+
+const getDir = dir => glob.sync(`${input_dir}/**/*.md`)
 
 let parse = R.compose(
 	content => R.assoc('body', marked(content.body), content),
@@ -27,7 +45,7 @@ const writeJsons = obj =>
 	R.sequence(
 		Task.of, 
 		R.map(key => 
-			writeFile('./', {filename:key+'.json' ,buffer:JSON.stringify(obj[key])}), 
+			writeFile(output_dir+'/', {filename:key+'.json' ,buffer:JSON.stringify(obj[key])}), 
 		Object.keys(obj))
 	)
 
